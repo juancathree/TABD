5955ca7583e62faa46a28917bbce140b896a4b10
germannrl <50140108+germannrl@users.noreply.github.com>
2020-04-22 12:56:27 +0200

    Delete paquete.sql

--- paquete.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE PROCEDURE Add_Videojuego(Titulo IN VARCHAR, Precio IN NUMBER, 
-                            Anio IN NUMBER, Dispositivos IN ListaDispositivos) IS
-BEGIN
-    
-    INSERT INTO Tabla_Videojuego VALUES(Videojuego(seqVideojuego.NEXTVAL,Titulo,Precio
-                                        ,Anio, Dispositivos));
-
-END;
-/
